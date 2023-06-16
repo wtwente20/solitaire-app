0ef87c4934d1767a8c367f8435795dc0690559dd
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2023-06-15 23:20:35 -0500

    minor commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { DragDropModule } from '@angular/cdk/drag-drop';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
